8118f0808560d0e659820a7e7bf29dabc8acb140
rubenalon1973 <rubenalonso1973@gmail.com>
2023-05-10 16:43:28 +0200

    Initial Commit

---
+++ iFastFood/iFastFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFastFoodApp.swift
+//  iFastFood
+//
+//  Created by Ruben Alonso on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iFastFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
